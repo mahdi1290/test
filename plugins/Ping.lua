6e49b2977b1937d282fc3b31de1aad77ee49c072
mahdi1290 <mahdi138165@gmail.com>
2017-04-23 15:55:25 +0430

    Add files via upload

---
+++ plugins/Ping.lua
@@ -0,0 +1,16 @@
+﻿--Begin Ping.lua By @sezarinfo
+local datebase = {
+   "انـلاینم و حـواسم بـه گـروه هـست❤️",
+  }
+local function run(msg, matches) 
+return datebase[math.random(#datebase)]
+end
+return {
+  patterns = {
+    "^([Pp][Ii][Nn][Gg])",
+	"^[!/#]([Pp][Ii][Nn][Gg])",
+	"^(انلاینی)"
+  },
+  run = run
+}
+--End Ping.lua--
